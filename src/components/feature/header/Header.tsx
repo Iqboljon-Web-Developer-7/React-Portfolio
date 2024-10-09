e79906b2e216243cd60cc8e6c5f9296a1d8e8e10
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-10-09 22:41:29 +0500

    dark mode toggler added

--- src/components/feature/header/Header.tsx
+++ src/components/feature/header/Header.tsx
@@ -25,7 +25,19 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
+import { Moon, Sun } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "@/pages/theme-provider";
+
 const Header = () => {
+  const { setTheme } = useTheme();
   // Mobile header
   const [isOpen, setIsOpen] = useState(false);
 
@@ -87,6 +99,27 @@ const Header = () => {
               </SheetHeader>
             </SheetContent>
           </Sheet>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={() => setTheme("light")}>
+                Light
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("dark")}>
+                Dark
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={() => setTheme("system")}>
+                System
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
           <RiMenu5Line
             className="cursor-pointer md:hidden text-2xl sm:text-3xl"
             onClick={() => setIsOpen((prev) => !prev)}
